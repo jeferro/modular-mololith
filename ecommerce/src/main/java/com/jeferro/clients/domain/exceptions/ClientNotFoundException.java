c8734f5d2aed9af483b1f6fbc98635918a2ab2ed
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-02-18 14:32:17 +0100

    feature: remove module clients in ecommerce

--- ecommerce/src/main/java/com/jeferro/clients/domain/exceptions/ClientNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.jeferro.clients.domain.exceptions;
-
-public class ClientNotFoundException extends RuntimeException {
-
-    public ClientNotFoundException(String id) {
-        super("Client not found with id: " + id);
-    }
-}
